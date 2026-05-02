1310f66970a68718ddda0948467ac209f916cc1e
Geva Haviv <geva98@gmail.com>
2026-05-02 18:58:06 +0300

    Add HumanPlayer class with constructor stub

---
+++ HumanPlayer.java
@@ -0,0 +1,24 @@
+/**
+ * Represents a human player in a Tic-Tac-Toe game.
+ * Input is received from the user via the keyboard.
+ *
+ * @author Geva Haviv
+ */
+public class HumanPlayer implements Player {
+
+	/**
+	 * Constructs a new human player.
+	 */
+	public HumanPlayer() {
+	}
+
+	/**
+	 * Plays a single turn by reading the desired position from the user.
+	 *
+	 * @param board the <code>Board</code> on which to play.
+	 * @param mark  the <code>Mark</code> assigned to this player.
+	 */
+	@Override
+	public void playTurn(Board board, Mark mark) {
+	}
+}
